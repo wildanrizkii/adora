6fdad89d035bf311130b72a7d7c82b5bb9eb239b
Wildan <wildanrizki9560@gmail.com>
2025-02-21 00:36:48 +0700

    First commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,39 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Adora",
+  description: "Apotek Adora",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="light">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import {
+  Menu,
+  X,
+  Home,
+  Info,
+  Briefcase,
+  Phone,
+  ArrowRightFromLine,
+} from "lucide-react";
+import { DefaultLayout } from "../Layout";
+
+const Dashboard = () => {
+  const { theme, setTheme } = useTheme();
+  const { forcedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const disabled = !!forcedTheme;
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <div>
+      <DefaultLayout />
+    </div>
+  );
+};
+
+export default Dashboard;
