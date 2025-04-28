b1b944716a581abede7549bd713503289f9f8876
Wildan Rizki Nurfauzi <wildanrizki9560@gmail.com>
2025-04-28 20:01:14 +0700

    Manual fix

--- src/components/Admin/ActivityLog/Login/index.jsx
+++ src/components/Admin/ActivityLog/Login/index.jsx
@@ -52,7 +52,7 @@ const logActivity = async ({ idUser, role, action, detail, userAgent }) => {
     ]);
 
     if (error) throw error;
-    // console.log("✅ Log aktivitas berhasil disimpan:", {
+    // console.log("Log aktivitas berhasil disimpan:", {
     //   idUser,
     //   action,
     //   role,
